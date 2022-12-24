e724cad2600d4f90cdd9cfffd588b4264bced335
Shorya Malani <shoryamal@gmail.com>
2022-12-24 00:58:35 -0600

    new net

--- api/startup.sh
+++ api/startup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # python3 update_data_hourly.py cron.log &
 python3 update_data_now.py cron.log &
-gunicorn -b :5000 api:app --workers 1 --timeout 15  &
+gunicorn -b 0.0.0.0:5000 api:app --workers 1 --timeout 15  &
 wait
