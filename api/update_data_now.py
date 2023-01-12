c7f753542d577975b481f38bc987c1da1c2aebbf
Shorya Malani <shoryamal@gmail.com>
2023-01-11 19:05:01 -0500

    new code for swerve

--- api/update_data_now.py
+++ api/update_data_now.py
@@ -2,7 +2,7 @@ import update_data_hourly
 from sys import argv
 cron_file = "cron.log"
 
-if(len(argv)>2):
-    cron_file = argv[2]
+if(len(argv)>1):
+    cron_file = argv[1]
 
 update_data_hourly.update_bills(cron_file)
