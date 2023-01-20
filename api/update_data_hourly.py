fdbb6e062c7f5ac2bc0e840e7cc0a9912b97ecc7
Shorya Malani <shoryamal@gmail.com>
2023-01-20 00:15:59 -0500

    moving to 118

--- api/update_data_hourly.py
+++ api/update_data_hourly.py
@@ -41,7 +41,7 @@ def update_bills(log_file):
     conn = dbs_worker.set_up_connection()
     for i in to_update[:25]:
         print(i)
-        dbs_worker.get_and_update_member_info(conn,i['id'],propublica_data=i)
+        dbs_worker.get_and_update_member_info(conn,i['id'] if 'id' in i else i['request']['bioguideId'],propublica_data=i)
     dbs_worker.rethink_bills(dbs_worker.set_up_connection())
         # dbs_worker.get_recent_info(dbs_worker.set_up_connection())
     congress_data_api.get_current_data() # gets new bill information
