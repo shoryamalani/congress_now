c6fc5c4d951623583e58d90bb7f1341974363268
Shorya Malani <shoryamal@gmail.com>
2023-12-30 20:47:58 -0500

    adding implementation

--- api/dbs_worker.py
+++ api/dbs_worker.py
@@ -550,6 +550,7 @@ def update_bills(conn,num):
     all_bills = get_all_bills_to_update(conn)
     final_bills = {}
     for bill in all_bills:
+        print(bill)
         if bill[0]['latestAction']['actionDate'] in final_bills:
             final_bills[bill[0]['latestAction']['actionDate']].append(bill)
         else:
